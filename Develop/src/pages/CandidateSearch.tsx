f9105182095a8826c3674179820b6aa2fba2490f
Daniel <Danieltwarg@outlook.com>
2025-02-06 17:40:46 -0500

    added useState const to use within my tsx

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -4,6 +4,12 @@ import Candidate from '../interfaces/Candidate.interface';
 import CandidateCard from '../components/candidateCard/candidateCard';
 
 const CandidateSearch = () => {
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+  const [potentialCandidates, setPotentialCandidates] = useState<Candidate[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+
   return <h1>CandidateSearch</h1>;
 };
 
