d978f97780e51c617979ea16a020c8e4439c1a24
Daniel <Danieltwarg@outlook.com>
2025-02-06 16:33:10 -0500

    set up Candidate interface

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,13 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    id: number;
+    name: string;
+    username: string;
+    location: string;
+    avatar: string;
+    email: string;
+    htmlURL: string;
+    company: string;
+    bio: string;
+}
+export default Candidate;
